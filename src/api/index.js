ebcbc8194b71eaa2d99d7218ff60a260e5d28a6e
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-04-26 22:31:27 -0300

    configure .env and api url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import * as users from "./users";
 import * as search from "./search";
 
-const baseURL = "http://localhost:8081/api";
+const fallbackApiUrl = "http://localhost:8081/api";
+const baseURL = process.env.REACT_APP_API_URL || fallbackApiUrl;
 
 export const api = axios.create({
   baseURL,
